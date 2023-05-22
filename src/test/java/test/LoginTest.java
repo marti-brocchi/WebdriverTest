5f0897b8b0348c053d4dedc15c12959a574cf78d
marti-brocchi <marti.brocchi@gmail.com>
2023-05-22 15:11:38 +0200

    initial commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import POs.LoginFormPO;
+import POs.LoginSuccessPO;
+
+
+public class LoginTest extends DriverLifeCycleSetting {
+	
+  private LoginFormPO login;
+  private LoginSuccessPO loginSuccess;
+  
+  @Test
+  public void testLoginOK() {
+	 login = new LoginFormPO(driver);
+	 loginSuccess = (LoginSuccessPO) login.with("user", "user"); 
+	 assertTrue(loginSuccess.successBoxIsPresent());
+  }
+  
+  @Test
+  public void testLoginNoOK() {
+	 login = new LoginFormPO(driver);
+	 login.with("user", "error"); 
+	 assertTrue(login.invalidBoxIsPresent());
+  }
+}
